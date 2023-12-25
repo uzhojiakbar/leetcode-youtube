15b81457b2b6650040aa869df926091f2de8f9b0
kali <kali@kali>
2023-12-25 18:28:21 -0500

    #1 - Two sum - easy

--- easy/TwoSum_1.js
+++ easy/TwoSum_1.js
@@ -1,3 +1,6 @@
+// #1 - Two Sum - easy
+// https://leetcode.com/problems/two-sum/
+
 const twoSum = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
